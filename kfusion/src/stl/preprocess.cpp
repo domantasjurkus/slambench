a2808245869952286b00695683c90b926dc9f74d
Dom Jurkus <domantas.jurkus@gmail.com>
2017-12-23 13:43:07 +0200

    Upade comments in preprocessing

--- kfusion/src/stl/preprocess.cpp
+++ kfusion/src/stl/preprocess.cpp
@@ -1,7 +1,5 @@
 #include <commons.h>
-// SyclInclude
 
-// STL
 void mm2metersKernel(std::vector<float> &out, uint2 outSize, const ushort * in, uint2 inSize) {
     if ((inSize.x < outSize.x) || (inSize.y < outSize.y))           { std::cerr << "Invalid ratio." << std::endl; exit(1); }
     if ((inSize.x % outSize.x != 0) || (inSize.y % outSize.y != 0)) { std::cerr << "Invalid ratio." << std::endl; exit(1); }
@@ -9,9 +7,8 @@ void mm2metersKernel(std::vector<float> &out, uint2 outSize, const ushort * in, 
     
     int ratio = inSize.x / outSize.x;
 
-    // No built-in parallel support yet - use 3rd parties
-    //exp::parallel::generate(std::experimental::parallel::par, out.begin(), out.end(), [x=0,y=0,in,inSize,outSize,ratio]() mutable {}
-    
+    // Subsampilng in parallel would result in inconsistent results as
+    // there would be data races fow computing the x and y values
     std::generate(out.begin(), out.end(), [x=0,y=0,in,inSize,outSize,ratio]() mutable {
         float ret = in[x*ratio + inSize.x*y*ratio] / 1000.0f;
 		x++;
@@ -21,16 +18,21 @@ void mm2metersKernel(std::vector<float> &out, uint2 outSize, const ushort * in, 
 		}
 		return ret;
     });
+
+    // for (y = 0; y < outSize.y; y++) {
+	// 	for (unsigned int x = 0; x < outSize.x; x++) {
+	// 		out[x + outSize.x * y] = in[x * ratio + inSize.x * y * ratio] / 1000.0f;
+	// 	}
+    // }
 }
 
-// Todo: STL
 void bilateralFilterKernel(std::vector<float> &out, const std::vector<float> in, uint2 size, const float * gaussian, float e_d, int r) {
     uint y;
     float e_d_squared_2 = e_d * e_d * 2;
 
     // Two options:
-    // 1. generate (iota) vectors for y and x values, then foreach loop
-    // 2. generate coordinate vector [(y,x),(y,x),(y,x)], then single foreach (paralelizable)
+    // 1. Generate all (x,y) pairs, then run a single foreach loop (paralelizable)
+    // 2. Paralellise only the inner loop
     for (y=0; y<size.y; y++) {
         for (uint x=0; x<size.x; x++) {
             uint pos = x + y*size.x;
@@ -44,14 +46,11 @@ void bilateralFilterKernel(std::vector<float> &out, const std::vector<float> in,
 
             const float center = in[pos];
 
-            // r will be 2 by default
-            // reduction to t and sum
-
-            // Use std::accummulate() to reduce to t
-            // Use std:accummulate() again to reduce to sum?
-
-            for (int i = -r; i <= r; i++) {
-                for (int j = -r; j <= r; j++) {
+            // We know the r value will be small (2 by default)
+            // Therefore it may be sensible to generate a vector
+            // of all (i,j) pairs, then loop std::for_each
+            for (int i=-r; i<=r; i++) {
+                for (int j=-r; j<=r; j++) {
                     uint2 curPos = make_uint2(clamp(x+i, 0u, size.x-1),
                                               clamp(y+j, 0u, size.y-1));
                     const float curPix = in[curPos.x + curPos.y*size.x];
@@ -63,49 +62,20 @@ void bilateralFilterKernel(std::vector<float> &out, const std::vector<float> in,
                     }
                 }
             }
+            // for (int i=-r; i<=r; i++) {
+            //     for (int j=-r; j<=r; j++) {
+            //         uint2 curPos = make_uint2(clamp(x+i, 0u, size.x-1),
+            //                                   clamp(y+j, 0u, size.y-1));
+            //         const float curPix = in[curPos.x + curPos.y*size.x];
+            //         if (curPix > 0) {
+            //             const float mod = sq(curPix - center);
+            //             const float factor = gaussian[i+r]*gaussian[j+r]*expf(-mod / e_d_squared_2);
+            //             t += factor * curPix;
+            //             sum += factor;
+            //         }
+            //     }
+            // }
             out[pos] = t / sum;
         }
     }
 }
-
-// Original
-/*void bilateralFilterKernel(float* out, const float* in, uint2 size, const float * gaussian, float e_d, int r) {
-    uint y;
-    float e_d_squared_2 = e_d * e_d * 2;
-
-    // STL the for loop?
-    // For each output value:
-    for (y = 0; y < size.y; y++) {
-        for (uint x = 0; x < size.x; x++) {
-            uint pos = x + y * size.x;
-            if (in[pos] == 0) {
-                out[pos] = 0;
-                continue;
-            }
-
-            float sum = 0.0f;
-            float t = 0.0f;
-
-            const float center = in[pos];
-
-            // r will be 2 by default
-            // reduction to t and sum
-            for (int i = -r; i <= r; ++i) {
-                for (int j = -r; j <= r; ++j) {
-                    uint2 curPos = make_uint2(clamp(x+i, 0u, size.x-1),
-                                              clamp(y+j, 0u, size.y-1));
-                    const float curPix = in[curPos.x + curPos.y * size.x];
-                    if (curPix > 0) {
-                        const float mod = sq(curPix - center);
-                        const float factor = gaussian[i+r]
-                            * gaussian[j+r]
-                            * expf(-mod / e_d_squared_2);
-                        t += factor * curPix;
-                        sum += factor;
-                    }
-                }
-            }
-            out[pos] = t / sum;
-        }
-    }
-}*/
