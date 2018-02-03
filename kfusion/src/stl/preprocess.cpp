f2157ffac47a5cbbc7d7ba9a9097c18d6058e83d
Dom Jurkus <domantas.jurkus@gmail.com>
2018-02-03 13:37:23 +0000

    A horrible, horrible way of doing bilateral filtering (code bloat, slower and loss of semantics, kept as comments)

--- kfusion/src/stl/preprocess.cpp
+++ kfusion/src/stl/preprocess.cpp
@@ -3,8 +3,9 @@
 //#include <experimental/algorithm>
 //#include <sycl/execution_policy>
 
-//#include <range/v3/algorithm/for_each.hpp>
-//#include <range/v3/view.hpp>
+/*namespace {
+    sycl::sycl_execution_policy<class bilateral_filter> bilateral_filter_par;
+}*/
 
 //void mm2metersKernel(std::vector<float> &out, uint2 outSize, const std::vector<ushort> in, uint2 inSize) {
 void mm2metersKernel(std::vector<float> &out, uint2 outSize, const ushort *in, uint2 inSize) {
@@ -18,7 +19,7 @@ void mm2metersKernel(std::vector<float> &out, uint2 outSize, const ushort *in, u
     // there would be data races when computing the x and y values
     // Also this looks ugly
 
-    // std::generate(out.begin(), out.end(), [x=0,y=0,in,inSize,outSize,ratio]() mutable {
+    // std::generate(out.begin(), out.end(), [x=0,y=0,=]() mutable {
     //     float ret = in[x*ratio + inSize.x*y*ratio] / 1000.0f;
 	// 	x++;
 	// 	if (x == outSize.x) {
@@ -57,107 +58,48 @@ void bilateralFilterKernel(std::vector<float> &out, const std::vector<float> in,
             }
 
             const float center = in[pos];
-            // float t = 0.0f;
-            // float sum = 0.0f;
+            float t = 0.0f;
+            float sum = 0.0f;
 
-            /*struct {
-                float t = 0.0f;
-                float sum = 0.0f;
-            } pair;*/
+            /*std::vector<int2> pairs = generate_int_pairs(-r,r,-r,r);
+            std::vector<std::pair<int2, float>> near_depths(pairs.size());
 
-            // After changing p to float2, ..
-            auto op = [&](float2 acc, int2 p) {
+            std::transform(pairs.begin(), pairs.end(), near_depths.begin(), [=](int2 p) {
                 uint2 curPos = make_uint2(clamp(x+p.x, 0u, size.x-1),
                                           clamp(y+p.y, 0u, size.y-1));
-                const float curPix = in[curPos.x + curPos.y*size.x];
-                if (curPix > 0) {
-                    const float mod = std::pow(curPix-center, 2);
-                    const float factor = gaussian[p.x+r]*gaussian[p.y+r]*expf(-mod / e_d_squared_2);
-                    acc.x += (factor * curPix);
+                return std::make_pair(p, in[curPos.x + curPos.y*size.x]);
+            });
+
+            auto reduce_neighbourhood = [&](float2 acc, std::pair<int2, float> p) {
+                if (p.second > 0) {
+                    const float mod = std::pow(p.second - center, 2);
+                    const float factor = gaussian[p.first.x+r]*gaussian[p.first.y+r] * expf(-mod / e_d_squared_2);
+                    acc.x += (factor * p.second);
                     acc.y += factor;
                 }
                 return acc;
             };
 
-            // Generate float pairs so that 
-            std::vector<int2> pairs = generate_int_pairs(-r,r,-r,r);
+            //auto result = std::accumulate(near_depths.begin(), near_depths.end(), make_float2(0.0f, 0.0f), reduce_neighbourhood);
+            auto result = std::experimental::parallel::reduce(bilateral_filter_par, near_depths.begin(), near_depths.end(),
+                                                              make_float2(0.0f, 0.0f), reduce_neighbourhood);
+            out[pos] = result.x / result.y;*/
 
-            
-
-            // If the operator for reduction is of two different types, it cannot be associative.
-
-            // Come up with a simple non-associative opeartor using int pairs
-
-            auto ret = std::accumulate(pairs.begin(), pairs.end(), make_float2(0.0f, 0.0f), op);
-
-            //sycl::sycl_execution_policy<class preprocess1> par1;
-            //sycl::sycl_execution_policy<class preprocess2> par2;
-
-            //std::experimental::parallel::for_each(par, pairs.begin(), pairs.end(), [=](int2 p) {
-            // std::for_each(pairs.begin(), pairs.end(), [&](int2 p) {
-            //     uint2 curPos = make_uint2(clamp(x+p.x, 0u, size.x-1),
-            //                               clamp(y+p.y, 0u, size.y-1));
-            //     const float curPix = in[curPos.x + curPos.y*size.x];
-            //     if (curPix > 0) {
-            //         //const float mod = sq(curPix - center);
-            //         const float mod = std::pow(curPix-center, 2);
-            //         const float factor = gaussian[p.x+r]*gaussian[p.y+r]*expf(-mod / e_d_squared_2);
-            //         // t += factor * curPix;
-            //         // sum += factor;
-            //         pair.add(factor, curPix);
-            //     }
-            // });
-
-
-            // std::vector<float> hamster = {1.2f, 3.4f, 5.6f, 7.8f};
-            // sycl::sycl_execution_policy<class preprocess3> par3;
-
-            // t = std::experimental::parallel::reduce(par3, pairs.begin(), pairs.end(), 0.0f, [](float acc, int2 p) {
-            //     return acc;
-            // });
-
-            // Bad (?) idea: reduce i and sum separately
-            // t = std::experimental::parallel::reduce(par1, pairs.begin(), pairs.end(), 0.0f, [=](float acc, int2 p) {
-            //     return 1.0f;
-            //     // uint2 curPos = make_uint2(clamp(x+p.x, 0u, size.x-1),
-            //     //                           clamp(y+p.y, 0u, size.y-1));
-            //     // const float curPix = in[curPos.x + curPos.y*size.x];
-            //     // if (curPix > 0) {
-            //     //     const float mod = sq(curPix - center);
-            //     //     const float factor = gaussian[p.x+r]*gaussian[p.y+r]*expf(-mod / e_d_squared_2);
-            //     //     return acc + factor*curPix;
-            //     // }
-            //     // return 0.0f;
-            // });
-
-            // sum = std::accumulate(pairs.begin(), pairs.end(), 0.0f, [&](float acc, int2 p) {
-            //     uint2 curPos = make_uint2(clamp(x+p.x, 0u, size.x-1),
-            //                               clamp(y+p.y, 0u, size.y-1));
-            //     const float curPix = in[curPos.x + curPos.y*size.x];
-            //     if (curPix > 0) {
-            //         const float mod = sq(curPix - center);
-            //         const float factor = gaussian[p.x+r]*gaussian[p.y+r]*expf(-mod / e_d_squared_2);
-            //         return acc + factor;
-            //     }
-            //     return acc;
-            // });
-            out[pos] = ret.x / ret.y; //t / sum;
-            
-            // Original
-            //
-            // for (int i=-r; i<=r; i++) {
-            //     for (int j=-r; j<=r; j++) {
-            //         uint2 curPos = make_uint2(clamp(x+i, 0u, size.x-1),
-            //                                   clamp(y+j, 0u, size.y-1));
-            //         const float curPix = in[curPos.x + curPos.y*size.x];
-            //         if (curPix > 0) {
-            //             const float mod = sq(curPix - center);
-            //             const float factor = gaussian[i+r]*gaussian[j+r]*expf(-mod / e_d_squared_2);
-            //             t += factor * curPix;
-            //             sum += factor;
-            //         }
-            //     }
-            // }
+            // Original            
+            for (int i=-r; i<=r; i++) {
+                for (int j=-r; j<=r; j++) {
+                    uint2 curPos = make_uint2(clamp(x+i, 0u, size.x-1),
+                                              clamp(y+j, 0u, size.y-1));
+                    const float curPix = in[curPos.x + curPos.y*size.x];
+                    if (curPix > 0) {
+                        const float mod = sq(curPix - center);
+                        const float factor = gaussian[i+r]*gaussian[j+r]*expf(-mod / e_d_squared_2);
+                        t += factor * curPix;
+                        sum += factor;
+                    }
+                }
+            }
+            out[pos] = t/sum;
         });
     });
 }
